e1480c9afa75d43a7f8985e8a262c672f70e89b6
132nd-etcher <132nd-etcher@daribouca.net>
2018-03-03 11:11:35 +0100

    validate config

--- emft/config/_config.py
+++ emft/config/_config.py
@@ -2,10 +2,14 @@
 """
 EMFT config
 """
-
+from everett import ConfigurationError, ConfigurationMissingError, DetailedConfigurationError, InvalidKeyError, \
+    InvalidValueError
 from pathlib import Path
+import inspect
+import click
 
 import elib
+from emft.exit_ import exit_
 
 
 class _Val(elib.config.ConfigProp):
@@ -23,8 +27,28 @@ class Config(elib.config.BaseConfig):
     """
 
     debug: bool = _Val(bool, default='false')
-    source_folder: bool = _Val(Path, default='false')
+    source_folder: bool = _Val(Path)
     appveyor: str = _Val(str)
+
+    def _validate(self):
+        try:
+            for name, value in inspect.getmembers(self, inspect.isdatadescriptor):
+                print(name)
+        except ConfigurationMissingError as exc:
+            click.secho(f'Missing configuration value: "{exc.key}"', err=True, color='red')
+            click.echo('See README for more information.')
+            exit_(1)
+        except InvalidValueError as exc:
+            click.secho(f'Invalid configuration value for: "{exc.key}"', err=True, color='red')
+            click.echo('See README for more information.')
+            exit_(1)
+
 
     def __init__(self):
         elib.config.BaseConfig.__init__(self, 'EMFT')
+        self._validate()
+        # except ConfigurationMissingError as exc:
+        #     print(exc.key, 'missing')
+        #     raise
+
+
